115ae5b48d819ced256d1337780d9e4e93fc315e
Reaper202014 <149695272+Reaper202014@users.noreply.github.com>
2025-03-05 23:24:24 +0700

    Определите базовый класс Animal, который представляет животное и который содержит две приватные переменные: строку для хранения имени животного и целое число для хранения веса животного. Также определите общедоступную функцию print, которая выводит на консоль сообщение с указанием имени и веса объекта Animal. Также добавьте произвольную переменную с типом protected. Также создайте два производных класса Cat и Dog, которые наследуются от класса Animal. В функции main создайте несколько объектов типа Cat и Dog и с помощью функции print выведите информацию об этих объектах на консоль.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+private:
+    string name;
+    int weight;
+
+protected:
+    string type;
+
+public:
+    Animal(string nm, int wgt) : name(nm), weight(wgt) {}
+
+    void print() {
+        cout << "Имя: " << name << "Вес: " << weight << " кг." << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    Cat(string nm, int wgt) : Animal(nm, wgt) {
+        type = "Кошка";
+    }
+
+    void print() {
+        Animal::print();
+        cout << "Тип: " << type << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    Dog(string nm, int wgt) : Animal(nm, wgt) {
+        type = "Собака";
+    }
+
+    void print() {
+        Animal::print();
+        cout << "Тип: " << type << endl;
+    }
+};
+
+int main() {
+    Cat myCat("Мурка", 4);
+    Dog myDog("Шарик", 10);
+
+    myCat.print();
+    myDog.print();
+
+    return 0;
+}
