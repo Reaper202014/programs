d2e59343f404e18b437f4f5d962dd813923a22d2
Reaper202014 <149695272+Reaper202014@users.noreply.github.com>
2025-03-10 00:10:43 +0700

    Создайте два класса (минимум 2 числовых поля в каждом) и напишите дружественную функцию, которая будет складывать все поля из обоих классов друг с другом.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+class PerviKlass;
+
+class VtoroiKlass {
+private:
+    int A;
+    int B;
+
+public:
+    VtoroiKlass(int a, int b) : A(a), B(b) {}
+
+    friend int addValues(VtoroiKlass a, PerviKlass b);
+};
+
+class PerviKlass {
+private:
+    int H;
+    int Y;
+
+public:
+    PerviKlass(int c, int d) : H(c), Y(d) {}
+
+    friend int addValues(VtoroiKlass a, PerviKlass b);
+};
+
+int addValues(VtoroiKlass a, PerviKlass b) {
+    return a.A + a.B + b.H + b.Y;
+}
+
+int main() {
+    VtoroiKlass Pervoe(5, 10);
+    PerviKlass Vtoroe(3, 4);
+
+    int sum = addValues(Pervoe, Vtoroe);
+    cout << "Сумма всех чисел: " << sum << endl;
+
+    return 0;
+}
