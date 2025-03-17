86fc9fd06e8a6a742fecaa8b31966e8ef40b4fa2
Reaper202014 <149695272+Reaper202014@users.noreply.github.com>
2025-03-17 10:31:02 +0700

    Реализуйте базовый класс Vehicle описывает общие свойства всех транспортных средств, а производные классы Car и Bike. Необходимо реализовать производные классы Car и Bike, которые выделяют динамическую память и создать массив указателей на Vehicle и динамически выделить объекты Car и Bike. Оснастить логгированием.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+public:
+    virtual void printInfo() const = 0;
+    virtual ~Vehicle() {}
+};
+
+class Car : public Vehicle {
+public:
+    void printInfo() const override {
+        cout << "Это автомобиль" << endl;
+    }
+    ~Car() {
+        cout << "Автомобиль сломан" << endl;
+    }
+};
+
+class Bike : public Vehicle {
+public:
+    void printInfo() const override {
+        cout << "Это велосипед" << endl;
+    }
+    ~Bike() {
+        cout << "Велосипед сломан" << endl;
+    }
+};
+
+int main() {
+    const int size = 5;
+    Vehicle* vehicles[size];
+
+    for (int i = 0; i < size; ++i) {
+        if (i % 2 == 0) {
+            vehicles[i] = new Car();
+        }
+        else {
+            vehicles[i] = new Bike();
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        vehicles[i]->printInfo();
+    }
+
+    for (int i = 0; i < size; ++i) {
+        delete vehicles[i];
+    }
+
+    return 0;
+}
