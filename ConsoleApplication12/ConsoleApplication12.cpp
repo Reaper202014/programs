1fa5969104c0757e1fe89cab800c69c3fcedae51
Reaper202014 <149695272+Reaper202014@users.noreply.github.com>
2025-03-20 02:59:06 +0700

    Реализуйте простейший генератор указателей. Создайте объект, на который будет указывать одновременно несколько указателей. Количество указателей задает пользователь. Выведите в консоль адресы этих указателей.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cout << "Введите количество указателей: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Количество указателей должно быть положительным." << endl;
+        return 1;
+    }
+
+    int* object = new int(42);
+
+    int** pointers = new int* [n];
+
+    for (int i = 0; i < n; ++i) {
+        pointers[i] = object;
+    }
+
+    cout << "Адреса указателей:" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "Pointer " << i + 1 << ": " << pointers[i] << endl;
+    }
+
+    delete pointers;
+    delete object;
+
+    return 0;
+}
